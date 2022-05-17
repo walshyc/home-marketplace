682ce9b5ea66e9bac5b36b24d792b3952eeaec1d
CONOR GERARD WALSH <conor.a.walsh.2021@mumail.ie>
2022-05-17 15:47:13 +0100

    added navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import ForgotPassword from './pages/ForgotPassword';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
-    <div className="">
-      <h1>my App</h1>
-
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore></Explore>}></Route>
+          <Route path='/offers' element={<Offers></Offers>}></Route>
+          <Route path='/profile' element={<SignIn></SignIn>}></Route>
+          <Route path='/sign-in' element={<SignIn></SignIn>}></Route>
+          <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
+          <Route path='/forgot-password' element={<ForgotPassword></ForgotPassword>}></Route>
+        </Routes>
+        <Navbar></Navbar>
+      </Router>
+    </>
   );
 }
 
