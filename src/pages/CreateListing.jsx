bcc8e5e309f4a835b315cc2b2a943fd3b0488004
CONOR GERARD WALSH <conor.a.walsh.2021@mumail.ie>
2022-05-26 11:37:28 +0100

    added quick change

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -132,7 +132,7 @@ const CreateListing = () => {
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
+
       console.log(geolocation, location);
     }
     // function to store images in firebase
@@ -191,10 +191,9 @@ const CreateListing = () => {
       geolocation,
       timestamp: serverTimestamp(),
     };
-
+    formDataCopy.location = address;
     delete formDataCopy.images;
     delete formDataCopy.address;
-    location && (formDataCopy.location = location);
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
